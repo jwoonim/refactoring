f7452a394b4602ae009c0c7105c73a0976537493
임재운 <jwoonim90@gmail.com>
2022-02-08 16:51:54 +0900

    refactor: move amount for function

--- chapter_01/getLoyalties.js
+++ chapter_01/getLoyalties.js
@@ -5,7 +5,7 @@ const plays = require('./plays.json')
 renderPlainText = (data, plays) => {
     let result = `청구 내역 (고객명): ${data.customer}\n`
     for (let perf of data.performances) {
-        result += `${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
+        result += `${perf.play.name}: ${usd(perf.amountFor)} (${perf.audience}석)\n`
     }
     result += `총액: ${usd(totalAmount())}\n`
     result += `적립 포인트: ${totalVolumeCredits()}점\n`
@@ -14,7 +14,7 @@ renderPlainText = (data, plays) => {
     function totalAmount() {
         let result = 0
         for (let perf of data.performances) {
-            result += amountFor(perf)
+            result += perf.amountFor
         }
         return result
     }
@@ -38,6 +38,23 @@ renderPlainText = (data, plays) => {
             result += Math.floor(aPerformance.audience / 5)
         return result
     }
+}
+
+statement = (invoice, plays) => {
+    const statementData = {}
+    statementData.customer = invoice.customer
+    statementData.performances = invoice.performances.map(enrichPerformance)
+    return renderPlainText(statementData, plays)
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance)
+        result.play = playFor(result)
+        result.amountFor = amountFor(result)
+        return result
+    }
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
+    }
     function amountFor(aPerformance) {
         let result = 0
     
@@ -59,22 +76,6 @@ renderPlainText = (data, plays) => {
                 throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
         }
         return result
-    }
-}
-
-statement = (invoice, plays) => {
-    const statementData = {}
-    statementData.customer = invoice.customer
-    statementData.performances = invoice.performances.map(enrichPerformance)
-    return renderPlainText(statementData, plays)
-
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({}, aPerformance)
-        result.play = playFor(result)
-        return result
-    }
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID]
     }
 }
 
