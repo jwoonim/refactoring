681569302da834e1f2070c6aaf2269f6f103f5de
임재운 <jwoonim90@gmail.com>
2022-02-08 15:53:05 +0900

    refactor: extract inside statement function

--- chapter_01/getLoyalties.js
+++ chapter_01/getLoyalties.js
@@ -1,74 +1,81 @@
 const invoices = require('./invoices.json')
 const plays = require('./plays.json')
 
-usd = (aNumber) => {
-    return new Intl.NumberFormat('en-Us', {
-        style: 'currency', currency: 'USD', minimumFractionDigits: 2
-    }).format(aNumber/100)
-}
 
-volumeCreditsFor = (aPerformance) => {
-    let result = 0
-    result += Math.max(aPerformance.audience = 30, 0)
-    if ('comedy' === playFor(aPerformance).type)
-        result += Math.floor(aPerformance.audience / 5)
-    return result
-}
-
-totalVolumeCredits = (invoice) => {
-    let result = 0
-    for (let perf of invoice.performances) {
-        // 포인트 적립
-        result += volumeCreditsFor(perf)
-    }
-    return result
-}
-
-totalAmount = (invoice) => {
-    let result = 0
-    for (let perf of invoice.performances) {
-        result += amountFor(perf)
-    }
-    return result
-}
-
-amountFor = (aPerformance) => {
-    let result = 0
-
-    switch (playFor(aPerformance).type) {
-        case 'tragedy': // 비극
-        result = 40000
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30)
-            }
-            break
-        case 'comedy': // 희극
-            result = 30000
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20)
-            }
-            result += 300 * aPerformance.audience
-            break
-        default:
-            throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
-    }
-    return result
-}
-
-playFor = (aPerformance) => {
-    return plays[aPerformance.playID]
-}
-
-statement = (invoice, plays) => {
+renderPlainText = (invoice, plays) => { // 본문 전체를 별도 함수로 추출
     let result = `청구 내역 (고객명): ${invoice.customer}\n`
-
     for (let perf of invoice.performances) {
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
     }
-
     result += `총액: ${usd(totalAmount(invoice))}\n`
     result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`
     return result
+
+    function totalAmount(invoice) {
+        let result = 0
+        for (let perf of invoice.performances) {
+            result += amountFor(perf)
+        }
+        return result
+    }
+    function totalVolumeCredits(invoice) {
+        let result = 0
+        for (let perf of invoice.performances) {
+            // 포인트 적립
+            result += volumeCreditsFor(perf)
+        }
+        return result
+    }
+    function usd(aNumber) {
+        return new Intl.NumberFormat('en-Us', {
+            style: 'currency', currency: 'USD', minimumFractionDigits: 2
+        }).format(aNumber/100)
+    }
+    function volumeCreditsFor(aPerformance) {
+        let result = 0
+        result += Math.max(aPerformance.audience = 30, 0)
+        if ('comedy' === playFor(aPerformance).type)
+            result += Math.floor(aPerformance.audience / 5)
+        return result
+    }
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
+    }
+    function amountFor(aPerformance) {
+        let result = 0
+    
+        switch (playFor(aPerformance).type) {
+            case 'tragedy': // 비극
+            result = 40000
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30)
+                }
+                break
+            case 'comedy': // 희극
+                result = 30000
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20)
+                }
+                result += 300 * aPerformance.audience
+                break
+            default:
+                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
+        }
+        return result
+    }
+}
+
+statement = (invoice, plays) => {
+    // let result = `청구 내역 (고객명): ${invoice.customer}\n`
+
+    // for (let perf of invoice.performances) {
+    //     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
+    // }
+
+    // result += `총액: ${usd(totalAmount(invoice))}\n`
+    // result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`
+    // return result
+    return renderPlainText(invoice, plays) // 본문 전체를 별도 함수로 추출
 }
 
 console.log(statement(invoices[0], plays))
