8521fbe7b69fe68442398d0982f5d7b5e5a651cc
임재운 <jwoonim90@gmail.com>
2022-02-08 16:19:59 +0900

    refactor: remove play for function

--- chapter_01/getLoyalties.js
+++ chapter_01/getLoyalties.js
@@ -5,7 +5,7 @@ const plays = require('./plays.json')
 renderPlainText = (data, plays) => {
     let result = `청구 내역 (고객명): ${data.customer}\n`
     for (let perf of data.performances) {
-        result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
+        result += `${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
     }
     result += `총액: ${usd(totalAmount())}\n`
     result += `적립 포인트: ${totalVolumeCredits()}점\n`
@@ -34,17 +34,14 @@ renderPlainText = (data, plays) => {
     function volumeCreditsFor(aPerformance) {
         let result = 0
         result += Math.max(aPerformance.audience = 30, 0)
-        if ('comedy' === playFor(aPerformance).type)
+        if ('comedy' === aPerformance.play.type)
             result += Math.floor(aPerformance.audience / 5)
         return result
-    }
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID]
     }
     function amountFor(aPerformance) {
         let result = 0
     
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case 'tragedy': // 비극
             result = 40000
                 if (aPerformance.audience > 30) {
@@ -59,7 +56,7 @@ renderPlainText = (data, plays) => {
                 result += 300 * aPerformance.audience
                 break
             default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`)
+                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
         }
         return result
     }
