7cd9df9e8dbf03f804df523f81d96997c8a19952
임재운 <jwoonim90@gmail.com>
2022-02-08 17:38:45 +0900

    refactor: move total amount function

--- chapter_01/getLoyalties.js
+++ chapter_01/getLoyalties.js
@@ -7,17 +7,10 @@ renderPlainText = (data, plays) => {
     for (let perf of data.performances) {
         result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`
     }
-    result += `총액: ${usd(totalAmount())}\n`
+    result += `총액: ${usd(data.totalAmount)}\n`
     result += `적립 포인트: ${totalVolumeCredits()}점\n`
     return result
 
-    function totalAmount() {
-        let result = 0
-        for (let perf of data.performances) {
-            result += perf.amount
-        }
-        return result
-    }
     function totalVolumeCredits() {
         let result = 0
         for (let perf of data.performances) {
@@ -36,6 +29,7 @@ statement = (invoice, plays) => {
     const statementData = {}
     statementData.customer = invoice.customer
     statementData.performances = invoice.performances.map(enrichPerformance)
+    statementData.totalAmount = totalAmount(statementData)
     return renderPlainText(statementData, plays)
 
     function enrichPerformance(aPerformance) {
@@ -75,6 +69,13 @@ statement = (invoice, plays) => {
         result += Math.max(aPerformance.audience - 30, 0)
         if ('comedy' === aPerformance.play.type)
             result += Math.floor(aPerformance.audience / 5)
+        return result
+    }
+    function totalAmount(data) {
+        let result = 0
+        for (let perf of data.performances) {
+            result += perf.amount
+        }
         return result
     }
 }
