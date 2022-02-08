5b8ab6a350c0c7f5a6e166a43653985b3644c5ec
임재운 <jwoonim90@gmail.com>
2022-02-08 14:45:40 +0900

    refactor: change format function declaration

--- chapter_01/getLoyalties.js
+++ chapter_01/getLoyalties.js
@@ -1,11 +1,10 @@
 const invoices = require('./invoices.json')
 const plays = require('./plays.json')
 
-// 함수 변수를 일반 함수로 변경
-format = (aNumber) => {
+usd = (aNumber) => {
     return new Intl.NumberFormat('en-Us', {
         style: 'currency', currency: 'USD', minimumFractionDigits: 2
-    }).format(aNumber)
+    }).format(aNumber/100)  // 단위 변환 로직도 이 함수 안으로 이동
 }
 
 volumeCreditsFor = (aPerformance) => {
@@ -21,24 +20,19 @@ statement = (invoice, plays) => {
         volumeCredits = 0,
         result = `청구 내역 (고객명): ${invoice.customer}\n`
 
-    // const format = new Intl.NumberFormat('en-Us', {
-    //     style: 'currency', currency: 'USD', minimumFractionDigits: 2
-    // }).format
-
     for (let perf of invoice.performances) {
         // 포인트 적립
         volumeCredits += volumeCreditsFor(perf)
 
         // 청구 내역을 출력한다.
-        result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석\n)`
+        result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석\n)` // 함수 이름 변경
         totalAmount += amountFor(perf)
     }
-    result += `총액: ${format(totalAmount/100)}\n` // 임시 변수였던 format을 함수 호출로 대체
+    result += `총액: ${usd(totalAmount)}\n`
     result += `적립 포인트: ${volumeCredits}점\n`
     return result
 }
 
-// Nested function
 amountFor = (aPerformance) => {
     let result = 0
 
