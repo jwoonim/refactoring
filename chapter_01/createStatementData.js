73dc03e84cc2cd42f5eebe85d7888d5373edffb3
임재운 <jwoonim90@gmail.com>
2022-02-08 20:35:28 +0900

    refactor: replace constructor with factory function

--- chapter_01/createStatementData.js
+++ chapter_01/createStatementData.js
@@ -7,25 +7,15 @@ export default function createStatementData(invoice, plays) {
     return result
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance))
         const result = Object.assign({}, aPerformance)
         result.play = calculator.play
         result.amount = calculator.amount
-        result.volumeCredits = volumeCreditsFor(result)
+        result.volumeCredits = calculator.volumeCredits
         return result
     }
     function playFor(aPerformance) {
         return plays[aPerformance.playID]
-    }
-    function amountFor(aPerformance) {
-        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount
-    }
-    function volumeCreditsFor(aPerformance) {
-        let result = 0
-        result += Math.max(aPerformance.audience - 30, 0)
-        if ('comedy' === aPerformance.play.type)
-            result += Math.floor(aPerformance.audience / 5)
-        return result
     }
     function totalAmount(data) {
         return data.performances.reduce((total, p) => total + p.amount, 0)
@@ -35,6 +25,9 @@ export default function createStatementData(invoice, plays) {
     }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+    return new PerformanceCalculator(aPerformance, aPlay)
+}
 class PerformanceCalculator {
     constructor(aPerformance, aPlay) {
         this.performance = aPerformance
