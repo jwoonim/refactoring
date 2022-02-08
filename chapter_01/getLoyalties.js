a4f37da63972578b62de3be6a5a5e50e90bf1cb8
임재운 <jwoonim90@gmail.com>
2022-02-08 17:46:53 +0900

    refactor: replace for loop with pipeline

--- chapter_01/getLoyalties.js
+++ chapter_01/getLoyalties.js
@@ -66,18 +66,20 @@ statement = (invoice, plays) => {
         return result
     }
     function totalAmount(data) {
-        let result = 0
-        for (let perf of data.performances) {
-            result += perf.amount
-        }
-        return result
+        // let result = 0
+        // for (let perf of data.performances) {
+        //     result += perf.amount
+        // }
+        // return result
+        return data.performances.reduce((total, p) => total + p.amount, 0) // for 반복문을 파이프라인으로 바꿈
     }
     function totalVolumeCredits(data) {
-        let result = 0
-        for (let perf of data.performances) {
-            result += perf.volumeCredits
-        }
-        return result
+        // let result = 0
+        // for (let perf of data.performances) {
+        //     result += perf.volumeCredits
+        // }
+        // return result
+        return data.performances.reduce((total, p) => total + p.volumeCredits, 0) // for 반복문을 파이프라인으로 바꿈
     }
 }
 
