c134866c765f594c223b0532c6b0164363a49ebf
임재운 <jwoonim90@gmail.com>
2022-02-08 19:02:02 +0900

    refactor: update render HTML function

--- chapter_01/statements.js
+++ chapter_01/statements.js
@@ -9,12 +9,16 @@ function htmlStatement(invoice, plays) {
 }
 
 function renderHtml(data) {
-    let result = `청구 내역 (고객명): ${data.customer}\n`
+    let result = `<h1>청구 내역 (고객명): ${data.customer}</h1>\n`
+    result += `<table>\n`
+    result += `<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>`
     for (let perf of data.performances) {
-        result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`
+        result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>\n`
+        result += `<td>${usd(perf.amount)}</td></tr>\n`
     }
-    result += `총액: ${usd(data.totalAmount)}\n`
-    result += `적립 포인트: ${data.totalVolumeCredits}점\n`
+    result += `</table>\n`
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}점</em></p>\n`
     return result
 }
 
